c14f3af982ab077f6fb33de232eeb53c44f623b3
hakeem <hakeem0114@gmail.com>
2023-06-03 21:44:28 -0400

    Header done

--- src/App.jsx
+++ src/App.jsx
@@ -20,16 +20,16 @@ function App() {
           />
         </div>
 
-          <box className='p-8' >
+          <article className='p-8' >
               <h1  className='text-5xl text-center text-white font-bolder mb-8' >
                       My Oracle
               </h1>
               
 
-              <form action="#" className='flex justify-center max-w-xl mx-auto ' >
+              <form action="#" className='flex justify-center max-w-xl mx-auto  sm:max-xl' >
                     
                     <input 
-                      className='w-full px-6 py-2  rounded-l-xl'
+                      className='w-full px-6 py-2  rounded-l-xl '
 
                       type="text" 
                       name='ipAdress' 
@@ -42,26 +42,38 @@ function App() {
                       <img src={searchArrow} alt="search arrow" />
                     </button>
               </form>
-          </box>
+          </article>
 
-          <box className='bg-white rounded-xl shadow p-8 m-12 flex justify-center gap-5 ' >
-              <div className='lg:border-r-l '>
-                  <h3>Ip Address </h3>
-                  <h2>192.23.174.101</h2>
+          <article className=' text-center bg-white rounded-xl shadow p-8 mx-20 grid grid-cols-1 gap-8 md:grid-cols-2 max-w-4xl text-left lg:grid-cols-4 max-w-6xl mx-10 lg:mx-auto' >
+              <div className='lg:border-r  shadow-stone-400 lg:border-stone-600 shadow-stone-300'>
+                  <h2 className='text-sm uppercase font-bold text-neutral-500 tracking-wider mb-3'
+                  >Ip Address</h2>
+
+                  <p  className='font-medium text-stone-900 text-lg -mx-2 md:text-xl xl:text-2xl'  
+                  >192.272.174.101</p>
               </div>
-              <div>
-                  <h3>Ip Address </h3>
-                  <h2>192.23.174.101</h2>
+              <div className='lg:border-r  lg:border-stone-600'>
+                  <h2 className='text-sm uppercase font-bold text-neutral-500 tracking-wider mb-3'
+                  >Location</h2>
+
+                  <p  className='font-medium text-stone-900 text-lg  md:text-xl xl:text-2xl'  
+                  >Toronto, ON</p>
               </div>
-              <div>
-                  <h3>Ip Address </h3>
-                  <h2>192.23.174.101</h2>
+              <div className='lg:border-r  lg:border-stone-600'>
+                  <h2 className='text-sm uppercase font-bold text-neutral-500 tracking-wider mb-3'
+                  >TimeZone</h2>
+
+                  <p  className='font-medium text-stone-900 text-lg  md:text-xl xl:text-2xl'  
+                  >EST</p>
               </div>
-              <div>
-                  <h3>Ip Address </h3>
-                  <h2>192.23.174.101</h2>
+              <div className=''>
+                  <h2 className='text-sm uppercase font-bold text-neutral-500 tracking-wider mb-3'
+                  >ISP</h2>
+
+                  <p  className='font-medium text-stone-900 text-lg  md:text-xl xl:text-2xl'  
+                  >Canadian XTRX Sattelite</p>
               </div>
-          </box>
+          </article>
 
 
 
